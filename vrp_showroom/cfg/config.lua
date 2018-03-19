c24e4455bc439745bd1ba32c19d1629d941e4085
Alex-Gabriel <alexmieila01@gmail.com>
2018-03-19 20:33:54 +0200

    Add files via upload

---
+++ vrp_showroom/cfg/config.lua
@@ -0,0 +1,11 @@
+local cfg = {}
+
+cfg.showgarage = {
+  vehicles = {
+    {name = "Blista", costs = 500, model = "blista"},
+    {name = "Brioso R/A", costs = 5010000, model = "brioso"},
+    {name = "Sultan RS", costs = 500, model = "sultanrs"}
+  }
+}
+
+return cfg
